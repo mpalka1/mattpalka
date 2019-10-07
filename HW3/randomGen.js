dfb8b77a7a38fe36779bb196cb43885de003d514
Matthew Palka <palkamatthew@gmail.com>
2019-10-06 20:11:19 -0500

    complete

--- HW3/randomGen.js
+++ HW3/randomGen.js
@@ -3,17 +3,16 @@ var data = {letter:['a', 'b', 'c', 'd', 'e', 'f','g','h','i','j','k','l','m','n'
 letterUP:['A', 'B', 'C', 'D', 'E', 'F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
 symbols:['!', '@', '#', '$'],
 numbers:['0','1','2','3','4','5','6','7','8','9']};
-var outPut = '';
-var passValue = document.getElementById("#pass");
-
 
 function createArray(){
+    // takes in user input to determine char needed
     var q1 = confirm('Do you want special Characters?');
     var q2= confirm('Do you want Numbers?');
     var q3 = confirm('Do you want UpperCase?');
     var newArray = [];
     var arrayPopulation = newArray.concat(data.letter);
 
+    // checks which categories are needed in pass
     if(q1===true){
         arrayPopulation = arrayPopulation.concat(data.symbols);
     }
@@ -31,35 +30,24 @@ function createPass(array) {
     var len = prompt('What is you pw length?');
     var arrayPass = [];
     // While there remain elements to shuffle...
+
     for(var i =0; i < len ; i++){
     // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * (array.length-1));
-
       var randVal = array[randomIndex];
       arrayPass.push(randVal);
       length-=1;
     }
-    // alert(arrayPass.join(""));
     var outPut = arrayPass.join("");
-    alert(outPut);
+    document.getElementById("pass").innerHTML = outPut;
+     return outPut;
+  } 
+    //copy newly created pasword into the clipboard 
+  function textToClipboard () {
+    //copy items with 'p' tag into the clipboard
+    var text = $('p').html();
+    //convert into an input file to execute the copy commany
+    var tempEl = $('<input>').val(text).appendTo('body').select();
+    document.execCommand('copy');
+    tempEl.remove();
   }
-  
-    // function copyPass() {
-//     /* Get the text field */
-     var copyText = document.querySelector(#passWd);
-  
-//     /* Select the text field */
-//     copyText.select();
-//     copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-  
-//     /* Copy the text inside the text field */
-//     document.execCommand("copy");
-  
-//     /* Alert the copied text */
-//     alert("Copied the text: " + copyText.value);
-//   }
-
-
-/* first event */
-// change this pw into a dynamic random pw
-//document.write(pw);
